30036b1f22d12e5c6b151d5136d98d51bcd546c8
Evan <evanlunceford@hwsrv-1298118.hostwindsdns.com>
2025-09-27 02:16:10 +0000

    Why did I accept this job?

--- solver.c
+++ solver.c
@@ -1,5 +1,5 @@
 #include "nqueens.h"
-
+//Just because you're a senior dev doesn't mean you are good at coding lol
 // Solve the N-Queens problem recursively
 bool solve_nqueens_util(int board[], int col, int n) {
     if (col >= n) {
